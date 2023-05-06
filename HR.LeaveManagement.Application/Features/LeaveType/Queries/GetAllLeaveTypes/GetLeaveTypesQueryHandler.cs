4de0c20f52e91d2e11c6d778f00f5aaaad264a6e
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-06 12:52:26 +0300

    Creates GetLeaveTypesQuery and GetLeaveTypesQueryHandler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDTO>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<List<LeaveTypeDTO>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+        {
+            var leaveTypes = await _leaveTypeRepository.GetAllAsync();
+            var leaveTypesDTO = _mapper.Map<List<LeaveTypeDTO>>(leaveTypes);
+            return leaveTypesDTO;
+        }
+    }
+}
