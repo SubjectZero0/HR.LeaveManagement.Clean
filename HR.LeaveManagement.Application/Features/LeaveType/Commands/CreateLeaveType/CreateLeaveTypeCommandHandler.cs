d7de4820f271d1d361c38ff0e86952446d3866f2
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-06 15:44:49 +0300

    Creates CreateLeaveTypeCommand & CreateLeaveTypeCommandHandler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, bool>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveTypeCommandHandler(IMapper mapper,
+                                             ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<bool> Handle(CreateLeaveTypeCommand request,
+                                 CancellationToken cancellationToken)
+        {
+            // validate data
+
+            //map data
+            var leaveTypeDB = _mapper.Map<Domain.LeaveType>(request);
+
+            //create entity
+            var isCreated = await _leaveTypeRepository.AddAsync(leaveTypeDB);
+
+            if (isCreated)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
