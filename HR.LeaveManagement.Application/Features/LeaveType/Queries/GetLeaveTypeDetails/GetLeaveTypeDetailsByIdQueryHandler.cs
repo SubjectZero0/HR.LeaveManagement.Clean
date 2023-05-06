bf3b33a52c6cdb189d612b683d210bdf8cf1ce02
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-06 15:43:40 +0300

    Creates GetLeaveTypeDetailsByIdQuery & GetLeaveTypeDetailsByIdQueryHandler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails
+{
+    public class GetLeaveTypeDetailsByIdQueryHandler
+        : IRequestHandler<GetLeaveTypeDetailsByIdQuery, LeaveTypeDetailsDTO>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public GetLeaveTypeDetailsByIdQueryHandler(IMapper mapper,
+                                                   ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<LeaveTypeDetailsDTO> Handle(GetLeaveTypeDetailsByIdQuery request,
+                                                      CancellationToken cancellationToken)
+        {
+            var details = await _leaveTypeRepository.GetByIdAsync(request.Id);
+            var detailsDTO = _mapper.Map<LeaveTypeDetailsDTO>(details);
+            return detailsDTO;
+        }
+    }
+}
