ecc818f73a71d3770bf94b01e6d2ce1055809fae
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-10 21:17:40 +0300

    Creates HRDbConext

---
+++ HR.LeaveManagement.Persistence/DatabaseContexts/HRDbContext.cs
@@ -0,0 +1,40 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContexts
+{
+    public class HRDbContext : DbContext
+    {
+        public HRDbContext(DbContextOptions<HRDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override sealed async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in base.ChangeTracker.Entries<BaseClass>()
+                .Where(q => q.State is EntityState.Added or EntityState.Modified))
+            {
+                if (entry.State is EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.UtcNow;
+                }
+                else
+                {
+                    entry.Entity.DateModified = DateTime.UtcNow;
+                }
+            }
+            return await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocation { get; set; }
+    }
+}
