fff4d373221c88dfd51edd6b143f06ba681c2f3c
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-13 14:53:24 +0300

    adds ExistsInDbAsync method

--- HR.LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -5,5 +5,7 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence
     public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
     {
         public Task<bool> IsLeaveTypeUnique(string name);
+
+        public Task<bool> ExistsInDbAsync(int id);
     }
 }
