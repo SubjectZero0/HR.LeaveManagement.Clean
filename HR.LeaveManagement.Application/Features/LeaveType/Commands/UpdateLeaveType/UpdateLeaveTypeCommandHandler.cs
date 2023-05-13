e331678ea2eac3b22f675a0167d5dd89615b0c44
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-13 14:52:35 +0300

    creates and uses validator service for updating a leave type.

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
@@ -8,29 +9,41 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeave
     {
         private readonly IMapper _mapper;
         private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IUpdateLeaveTypeValidatorService _updateLeaveTypeValidatorService;
 
         public UpdateLeaveTypeCommandHandler(IMapper mapper,
-                                             ILeaveTypeRepository leaveTypeRepository)
+                                             ILeaveTypeRepository leaveTypeRepository,
+                                             IUpdateLeaveTypeValidatorService updateLeaveTypeValidatorService)
         {
             this._mapper = mapper;
             this._leaveTypeRepository = leaveTypeRepository;
+            this._updateLeaveTypeValidatorService = updateLeaveTypeValidatorService;
         }
 
         public async Task<bool> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
         {
             var leaveTypeDB = await _leaveTypeRepository.GetByIdAsync(request.Id);
 
-            if (leaveTypeDB is not null)
+            if (leaveTypeDB is null)
             {
-                _mapper.Map(request, leaveTypeDB);
-                leaveTypeDB.DateModified = DateTime.Now;
+                throw new NotFoundException("Could not find leave type");
+            }
 
-                var isUpdated = await _leaveTypeRepository.UpdateAsync(leaveTypeDB);
+            var validationResult = await _updateLeaveTypeValidatorService.ValidateLeaveTypeUpdateAsync(request);
 
-                if (isUpdated)
-                {
-                    return true;
-                }
+            if (validationResult.Errors.Any())
+            {
+                throw new BadRequestException("Invalid Leave Type", validationResult);
+            }
+
+            _mapper.Map(request, leaveTypeDB);
+            leaveTypeDB.DateModified = DateTime.Now;
+
+            var isUpdated = await _leaveTypeRepository.UpdateAsync(leaveTypeDB);
+
+            if (isUpdated)
+            {
+                return true;
             }
 
             return false;
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeValidator.cs
@@ -0,0 +1,72 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeValidator : AbstractValidator<UpdateLeaveTypeCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._leaveTypeRepository = leaveTypeRepository;
+
+            RuleFor(p => p.Name)
+                 .NotEmpty()
+                 .WithMessage("Property must not be empty");
+
+            RuleFor(p => p.Name)
+               .NotNull()
+               .WithMessage("Property must not be empty");
+
+            RuleFor(p => p.Name)
+               .MinimumLength(3)
+               .WithMessage("Property must be more than 3 characters");
+
+            RuleFor(p => p.Name)
+               .MaximumLength(50)
+               .WithMessage("Property must not exceed 50 characters");
+
+            RuleFor(p => p.Name)
+               .Matches("^[^0-9]+$")
+               .WithMessage("Property must not contain any numbers");
+
+            RuleFor(p => p.DefaultDays)
+                .NotNull()
+                .WithMessage("Property cannot be empty");
+
+            RuleFor(p => p.DefaultDays)
+               .LessThan(100)
+               .WithMessage("Property cannot exceed 100");
+
+            RuleFor(p => p.DefaultDays)
+               .GreaterThan(1)
+               .WithMessage("Property cannot be less than 1");
+
+            RuleFor(p => p)
+                .MustAsync(ExistsAsync)
+                .WithMessage("LeaveType does not exist");
+
+            RuleFor(p => p)
+                .MustAsync(IsLeaveTypeUniqueAsync)
+                .WithMessage("LeaveType with that name already Exists");
+        }
+
+        private async Task<bool> ExistsAsync(UpdateLeaveTypeCommand command, CancellationToken cancellationToken)
+        {
+            return await _leaveTypeRepository.ExistsInDbAsync(command.Id);
+        }
+
+        private async Task<bool> IsLeaveTypeUniqueAsync(UpdateLeaveTypeCommand command, CancellationToken cancellationToken)
+        {
+            return await _leaveTypeRepository.IsLeaveTypeUnique(command.Name);
+        }
+    }
+}
