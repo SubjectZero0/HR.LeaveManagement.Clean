a28c755a54dfcd35561309fda95057e883d1e306
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-17 23:29:26 +0300

    Creates LeaveTypesService. Needs work

---
+++ HR.LeaveManagement.API/Services/LeaveTypesService.cs
@@ -0,0 +1,33 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using MediatR;
+
+namespace HR.LeaveManagement.API.Services
+{
+    public interface ILeaveTypesService
+    {
+        Task<List<LeaveTypeDTO>> GetAllLeaveTypesAsync();
+    }
+
+    public class LeaveTypesService : ILeaveTypesService
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveTypesService(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        public async Task<List<LeaveTypeDTO>> GetAllLeaveTypesAsync()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypesQuery());
+            return leaveTypes;
+        }
+
+        public async Task<LeaveTypeDetailsDTO> GetLeaveTypeByIdAsync(int id)
+        {
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailsByIdQuery(id));
+            return leaveType;
+        }
+    }
+}
