19500faad1097a1ec402e25c71e2111458838ef4
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-17 23:28:51 +0300

    Creates LeaveTypesController

---
+++ HR.LeaveManagement.API/Controllers/LeaveTypesConctroller.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesConctroller : ControllerBase
+    {
+        public LeaveTypesConctroller()
+        {
+        }
+    }
+}
