57339080e77f4241b79935156b9d172ba7b965cb
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-18 21:15:58 +0300

    Adds more crud methods to service and finishes leave type controller

---
+++ HR.LeaveManagement.API/Controllers/LeaveTypesController.cs
@@ -0,0 +1,60 @@
+﻿using HR.LeaveManagement.API.Services;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.API.Controllers
+{
+    [Route("api/leavetypes")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        private readonly ILeaveTypesService _leaveTypesService;
+
+        public LeaveTypesController(ILeaveTypesService leaveTypesService)
+        {
+            this._leaveTypesService = leaveTypesService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var leaveTypesDto = await _leaveTypesService.GetAllLeaveTypesAsync();
+
+            return Ok(leaveTypesDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var leaveTypeDto = await _leaveTypesService.GetLeaveTypeByIdAsync(id);
+
+            return Ok(leaveTypeDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateLeaveTypeCommand command)
+        {
+            var leaveTypeDTO = await _leaveTypesService.CreateLeaveTypeAsync(command);
+
+            return Created(nameof(GetById), leaveTypeDTO);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateLeaveTypeCommand command)
+        {
+            var leaveType = await _leaveTypesService.UpdateLeaveTypeAsync(command);
+
+            return Ok(leaveType);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteLeaveTypeCommand command)
+        {
+            await _leaveTypesService.DeleteLeaveTypeAsync(command);
+
+            return NoContent();
+        }
+    }
+}
--- HR.LeaveManagement.API/Services/LeaveTypesService.cs
+++ HR.LeaveManagement.API/Services/LeaveTypesService.cs
@@ -1,4 +1,8 @@
-﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using MediatR;
 
@@ -9,15 +13,23 @@ namespace HR.LeaveManagement.API.Services
         Task<List<LeaveTypeDTO>> GetAllLeaveTypesAsync();
 
         Task<LeaveTypeDetailsDTO> GetLeaveTypeByIdAsync(int id);
+
+        Task<LeaveTypeDTO> CreateLeaveTypeAsync(CreateLeaveTypeCommand command);
+
+        Task<LeaveTypeDTO> UpdateLeaveTypeAsync(UpdateLeaveTypeCommand command);
+
+        Task DeleteLeaveTypeAsync(DeleteLeaveTypeCommand command);
     }
 
     public class LeaveTypesService : ILeaveTypesService
     {
         private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
 
-        public LeaveTypesService(IMediator mediator)
+        public LeaveTypesService(IMediator mediator, IMapper mapper)
         {
             this._mediator = mediator;
+            this._mapper = mapper;
         }
 
         public async Task<List<LeaveTypeDTO>> GetAllLeaveTypesAsync()
@@ -30,6 +42,27 @@ namespace HR.LeaveManagement.API.Services
         {
             var leaveType = await _mediator.Send(new GetLeaveTypeDetailsByIdQuery(id));
             return leaveType;
+        }
+
+        public async Task<LeaveTypeDTO> CreateLeaveTypeAsync(CreateLeaveTypeCommand command)
+        {
+            var leaveType = await _mediator.Send(command);
+            var leaveTypeDto = _mapper.Map<LeaveTypeDTO>(leaveType);
+
+            return leaveTypeDto;
+        }
+
+        public async Task<LeaveTypeDTO> UpdateLeaveTypeAsync(UpdateLeaveTypeCommand command)
+        {
+            var leaveType = await _mediator.Send(command);
+            var leaveTypeDto = _mapper.Map<LeaveTypeDTO>(leaveType);
+
+            return leaveTypeDto;
+        }
+
+        public async Task DeleteLeaveTypeAsync(DeleteLeaveTypeCommand command)
+        {
+            await _mediator.Send(command);
         }
     }
 }
