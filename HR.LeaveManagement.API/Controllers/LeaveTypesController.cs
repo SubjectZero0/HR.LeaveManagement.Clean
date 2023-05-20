49c7aa4616397f0f3a17e221675608e5dd5a1b1e
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-05-20 18:48:44 +0300

    adds logger and changes paths. Update uses DTO

--- HR.LeaveManagement.API/Controllers/LeaveTypesController.cs
+++ HR.LeaveManagement.API/Controllers/LeaveTypesController.cs
@@ -1,7 +1,9 @@
 ﻿using HR.LeaveManagement.API.Services;
+using HR.LeaveManagement.Application.Contracts.Logger;
 using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
 using HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
 using HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HR.LeaveManagement.API.Controllers
@@ -11,49 +13,61 @@ namespace HR.LeaveManagement.API.Controllers
     public class LeaveTypesController : ControllerBase
     {
         private readonly ILeaveTypesService _leaveTypesService;
+        private readonly IAppLogger<LeaveTypesController> _appLogger;
 
-        public LeaveTypesController(ILeaveTypesService leaveTypesService)
+        public LeaveTypesController(ILeaveTypesService leaveTypesService, IAppLogger<LeaveTypesController> appLogger)
         {
             this._leaveTypesService = leaveTypesService;
+            this._appLogger = appLogger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            _appLogger.LogInformation("Attempting to get all Leave Types");
             var leaveTypesDto = await _leaveTypesService.GetAllLeaveTypesAsync();
 
+            _appLogger.LogInformation("Returning OK for all Leave Types");
             return Ok(leaveTypesDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
+            _appLogger.LogInformation("Attempting to get Leave Type with ID: {0}", id);
             var leaveTypeDto = await _leaveTypesService.GetLeaveTypeByIdAsync(id);
 
+            _appLogger.LogInformation("Returning OK for Leave Type with ID: {0}", id);
             return Ok(leaveTypeDto);
         }
 
-        [HttpPost]
+        [HttpPost("new")]
         public async Task<IActionResult> Create(CreateLeaveTypeCommand command)
         {
+            _appLogger.LogInformation("Attempting to create Leave Type");
             var leaveTypeDTO = await _leaveTypesService.CreateLeaveTypeAsync(command);
 
+            _appLogger.LogInformation("Returning Created for Leave Type with ID: {0}", leaveTypeDTO.Id);
             return Created(nameof(GetById), leaveTypeDTO);
         }
 
-        [HttpPut]
-        public async Task<IActionResult> Update(UpdateLeaveTypeCommand command)
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateLeaveTypeDTO updateLeaveTypeDTO)
         {
-            var leaveType = await _leaveTypesService.UpdateLeaveTypeAsync(command);
+            _appLogger.LogInformation("Attempting to update Leave Type with ID: {0}", id);
+            var leaveTypeDTO = await _leaveTypesService.UpdateLeaveTypeAsync(id, updateLeaveTypeDTO);
 
-            return Ok(leaveType);
+            _appLogger.LogInformation("Returning OK for Leave Type with ID: {0}", id);
+            return Ok(leaveTypeDTO);
         }
 
         [HttpDelete]
         public async Task<IActionResult> Delete(DeleteLeaveTypeCommand command)
         {
+            _appLogger.LogInformation("Attempting to delete Leave Type with ID: {0}", command.Id);
             await _leaveTypesService.DeleteLeaveTypeAsync(command);
 
+            _appLogger.LogInformation("Returning 204 for Leave Type with ID: {0}", command.Id);
             return NoContent();
         }
     }
